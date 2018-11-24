909aa96283f7c9ea0211418e0cb3a71e492256f0
Ubaid-Manzoor <deadhacker014@gmail.com>
2018-11-24 15:32:27 +0530

    adding assinement

---
+++ YoutubePlalistScrape/index.py
@@ -0,0 +1,37 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter - ")
+if len(url) < 1: url = 'https://www.youtube.com/user/nesoacademy/playlists'
+
+html = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+div = soup.find_all('div' , class_="name of class")
+
+print(len(div))
+#
+# print(soup)
+# position = int(input("Enter Position ")) - 1
+# iteration = int(input("Enter Iterations ")) + 1
+# for repeat in range(iteration):
+#     print(url)
+#     html = urllib.request.urlopen(url).read()
+#     soup = BeautifulSoup(html, 'html.parser')
+#
+#     tags = soup('a')
+#     links = list()
+#     for tag in tags:
+#         link = tag.get("href", None)
+#         if str(link).startswith('http'):
+#             links.append(link)
+#     if len(links) >= position:
+#         url = links[position]
+#     else:
+#         print("Links out of range")
